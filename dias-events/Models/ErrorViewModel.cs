c393c705b5fb994048a0af8f1b17b839d7df0d63
Kevin Bernfeld <kcbernfeld@gmail.com>
2018-02-07 19:42:19 -0500

    more setup

---
+++ dias-events/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dias_events.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
