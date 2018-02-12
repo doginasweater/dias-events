244290104748250427a7577436c7ffb82b744f96
Kevin Bernfeld <kbernfeld@ngpvan.com>
2018-02-12 11:25:51 -0500

    adding material-ui to vendor file

--- dias-events/webpack.config.vendor.js
+++ dias-events/webpack.config.vendor.js
@@ -14,6 +14,7 @@ module.exports = (env) => {
         },
         entry: {
             vendor: [
+                'material-ui',
                 'moment',
                 'react',
                 'react-dom',
