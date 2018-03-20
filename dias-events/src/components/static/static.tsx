1db327282540eef05ac9f96104b9ab639dff43df
Kevin Bernfeld <kcbernfeld@gmail.com>
2018-03-20 19:08:46 -0400

    minor layout changes

--- dias-events/src/components/static/static.tsx
+++ dias-events/src/components/static/static.tsx
@@ -11,13 +11,17 @@ class StaticInternal extends React.Component<any, any> {
         return (
             <Grid.PureG>
                 <Grid.PureU1>
-                    I'm a header
+                    <a href="/" className="unstyled-link">
+                        <h1>SCBWI June 2018</h1>
+                    </a>
                 </Grid.PureU1>
                 <Grid.PureU1>
                     <Grid.PureU14>
+                        <h3>Navigation</h3>
                         left menu
                     </Grid.PureU14>
                     <Grid.PureU34>
+                        <h3>Register</h3>
                         the rest
                     </Grid.PureU34>
                 </Grid.PureU1>
